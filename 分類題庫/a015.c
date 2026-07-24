75c354577064e18b529067ac1254ae52e54e58b7
moodysinker <wesleyhuang06@gmail.com>
2026-07-24 10:02:27 +0800

    Create a015.c

---
+++ 分類題庫/a015.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void){
+    int row, col;
+    while(scanf("%d%d", &row, &col) != EOF){
+        int matrix[110][110];
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                scanf("%d", &matrix[i][j]);
+            }
+        }
+
+        for(int j = 0; j < col; j++){
+            for(int i = 0; i < row; i++){
+                printf("%d", matrix[i][j]);
+                if(j == col - 1 && i == row - 1){
+                    break;
+                } else {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
